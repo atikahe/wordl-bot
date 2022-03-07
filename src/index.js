059a55d5cf97b371d4c70f5f33ac4f0722da845d
atikah.efendi <atikah.efendi@gramedia.id>
2022-03-07 21:27:49 +0700

    Use keyv for session

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
+const Keyv = require('keyv')
 const { Client, Intents } = require('discord.js')
-const internal = require('stream')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
-
+const session = new Keyv()
 require('dotenv').config()
+
+session.on('error', err => console.error('Keyv connection error:', err))
 
 client.once('ready', () => {
     console.log(`Beep beep I'm ready! Logged in as ${client.user.tag}`)
-    console.log(Intents.FLAGS.GUILDS)
 })
 
-client.on('interactionCreate', async (interaction) => {
+client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return
 
-    if (interaction.commandName.toLowerCase() === 'ping') {
+    const { commandName } = interaction
+
+    if (commandName === 'ping') {
         await interaction.reply('Pong!')
+    } else if (commandName === 'server') {
+        await interaction.reply(`Server Info. Name: ${interaction.guild.name}, Members: ${interaction.guild.memberCount}. 😀`)
+    } else if (commandName === 'user') {
+        await interaction.reply('User Info')
     }
 })
 
