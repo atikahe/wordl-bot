28d7e06846421e0a21e13cb04c813fdc6b557f9f
atikah.efendi <atikah.efendi@gramedia.id>
2022-03-09 12:03:53 +0700

    Add constants

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+module.export = Object.freeze({
+    // Game modes
+    NORMAL: 'normal',
+    MANIC: 'manic',
+    LEGEND: 'legend',
+
+    // Emoji colors
+    GREEN: 'green',
+    YELLOW: 'yellow',
+    BLACK: 'black',
+    WHITE: 'white'
+})
