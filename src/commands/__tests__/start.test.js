bf94269850dadc365e415d05917181ac7e734adc
atikah.efendi <atikah.efendi@gramedia.id>
2022-03-13 15:54:24 +0700

    Unit test for start command

---
+++ src/commands/__tests__/start.test.js
@@ -0,0 +1,82 @@
+const Keyv = require('keyv')
+
+const day = require('../../utils/day')
+const messages = require('../../utils/messages')
+
+const start = require('../start')
+
+describe('Start', () => {
+    beforeEach(() => {
+        jest.resetModules() // Clears cache
+    })
+
+    test('Start new game in a guild', async () => {
+        // Mock discord interaction
+        const interaction = {
+            'guild': {'id': 1},
+            'options': {
+                'mode': 'normal',
+                getString(str) {
+                    return this[str]
+                }
+            },
+            async reply(words) {
+                return Promise.resolve(words)
+            }
+        }
+
+        // TODO: Mock async storage
+        const session = new Keyv()
+
+        const res = await start.execute(interaction, session)
+        const activeSession = await session.get(`${interaction.guild.id}-${day()}`)
+        
+        expect(res).toBe(messages.GAME_STARTED)
+        expect(activeSession).toBeInstanceOf(Array)
+    }),
+    test('Fail when on going game exist', async () => {
+        const interaction = {
+            'guild': {'id': 1},
+            'options': {
+                'mode': 'normal',
+                getString(str) {
+                    return this[str]
+                }
+            },
+            async reply(words) {
+                return Promise.resolve(words)
+            }
+        }
+        const session = new Keyv()
+        await session.set(
+            `${interaction.guild.id}-${day()}`,
+            [{ mode: 'normal', active: true, guess: [] }]
+        )
+
+        const res = await start.execute(interaction, session)
+
+        expect(res).toBe(messages.GAME_ONGOING)
+        expect(session).toBe(session)
+    })
+
+    test('Fail when mode is not chosen', async () => {
+        const interaction = {
+            'guild': {'id': 1},
+            'options': {
+                'mode': null,
+                getString(str) {
+                    return this[str]
+                }
+            },
+            async reply(words) {
+                return Promise.resolve(words)
+            }
+        }
+        const session = new Keyv()
+
+        const res = await start.execute(interaction, session)
+
+        expect(res).toBe(messages.SELECT_MODE)
+        expect(session).toBe(session)
+    })
+})
