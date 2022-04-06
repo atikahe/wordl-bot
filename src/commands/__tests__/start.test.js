376f04222b0d88bd483c412d1175979c6cab8cc5
atikah.efendi <atikah.efendi@gramedia.id>
2022-04-06 19:31:07 +0700

    Update test script

--- src/commands/__tests__/start.test.js
+++ src/commands/__tests__/start.test.js
@@ -2,6 +2,8 @@ const Keyv = require('keyv')
 
 const day = require('../../utils/day')
 const messages = require('../../utils/messages')
+const { ONGOING, WIN } = require('../../utils/constants')
+const ModeConfigs = require('../../configs/mode.json')
 
 const start = require('../start')
 
@@ -15,7 +17,7 @@ describe('Start', () => {
         const interaction = {
             'guild': {'id': 1},
             'options': {
-                'mode': 'normal',
+                'mode': 'wordle',
                 getString(str) {
                     return this[str]
                 }
@@ -24,21 +26,22 @@ describe('Start', () => {
                 return Promise.resolve(words)
             }
         }
-
-        // TODO: Mock async storage
         const session = new Keyv()
+        const date = new Date().toISOString().split('T')[0]
 
-        const res = await start.execute(interaction, session)
-        const activeSession = await session.get(`${interaction.guild.id}-${day()}`)
+        const res = await start.execute({interaction, session})
+        const activeSession = await session.get(`${interaction.guild.id}:${date}`)
         
-        expect(res).toBe(messages.GAME_STARTED)
+        expect(res).toEqual(expect.stringContaining(messages.GAME_STARTED))
         expect(activeSession).toBeDefined()
     }),
+
     test('Fail when on going game exist', async () => {
+        const mode = 'wordle'
         const interaction = {
             'guild': {'id': 1},
             'options': {
-                'mode': 'normal',
+                'mode': mode,
                 getString(str) {
                     return this[str]
                 }
@@ -48,14 +51,18 @@ describe('Start', () => {
             }
         }
         const session = new Keyv()
+        const date = new Date().toISOString().split('T')[0]
+        const modeConfig = ModeConfigs.find(config => config.mode === mode)
+        const guessesID = `${interaction.guild.id}-${mode}-${day(modeConfig.start)}`
+
         await session.set(
-            `${interaction.guild.id}-${day()}`,
-            { active: true, id: `${interaction.guild.id}-normal-${day()}` }
+            `${interaction.guild.id}:${date}`,
+            { active: true, guessesID, mode, status: ONGOING }
         )
 
-        const res = await start.execute(interaction, session)
+        const res = await start.execute({interaction, session})
 
-        expect(res).toBe(messages.GAME_ONGOING)
+        expect(res).toEqual(expect.stringContaining(messages.GAME_ONGOING))
         expect(session).toBe(session)
     })
 
@@ -74,9 +81,62 @@ describe('Start', () => {
         }
         const session = new Keyv()
 
-        const res = await start.execute(interaction, session)
+        const res = await start.execute({interaction, session})
 
-        expect(res).toBe(messages.SELECT_MODE)
+        expect(res).toEqual(expect.stringContaining(messages.SELECT_MODE))
+        expect(session).toBe(session)
+    })
+
+    test('Coming soon message for future modes', async () => {
+        const mode = 'quordle'
+        const interaction = {
+            'guild': {'id': 1},
+            'options': {
+                'mode': mode,
+                getString(str) {
+                    return this[str]
+                }
+            },
+            async reply(words) {
+                return Promise.resolve(words)
+            }
+        }
+        const session = new Keyv()
+
+        const res = await start.execute({interaction, session})
+
+        expect(res).toEqual(expect.stringContaining(messages.COMING_SOON))
+    })
+
+    test('Fail when mode is already done', async () => {
+        const mode = 'wordle'
+        const interaction = {
+            'guild': {'id': 1},
+            'options': {
+                'mode': mode,
+                getString(str) {
+                    return this[str]
+                }
+            },
+            async reply(words) {
+                return Promise.resolve(words)
+            }
+        }
+        const session = new Keyv()
+        const date = new Date().toISOString().split('T')[0]
+        const modeConfig = ModeConfigs.find(config => config.mode === mode)
+        const guessesID = `${interaction.guild.id}-${mode}-${day(modeConfig.start)}`
+        const sessionID = `${interaction.guild.id}:${date}`
+
+        await session.set(
+            sessionID,
+            { active: false, guessesID, mode, status: WIN }
+        )
+        await session.set(guessesID, [])
+
+        const res = await start.execute({interaction, session})
+
+        expect(res).toEqual(expect.stringContaining(messages.GAME_DONE))
         expect(session).toBe(session)
     })
 })
