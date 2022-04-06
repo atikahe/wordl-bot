9ad15507d3d56e8f563fb21dd0ab501396a09a13
atikah.efendi <atikah.efendi@gramedia.id>
2022-04-06 19:29:57 +0700

    Use params in day function

--- src/utils/day.js
+++ src/utils/day.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-module.exports = () => {
-    const timeDifference = new Date().getTime() - (process.env.START_DAY * 1000)
+module.exports = (start_day) => {
+    const timeDifference = new Date().getTime() - (start_day * 1000)
     return Math.ceil(timeDifference / (1000 * 3600 * 24))
 }
