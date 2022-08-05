d69bed039ad61bcba6cf09fd0e801a89b1be7049
atikah.efendi <atikah.efendi@gramedia.id>
2022-08-05 22:15:23 +0700

    Open command globally

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -17,12 +17,6 @@ for (const file of commandFiles) {
 const rest = new REST({version: '9'}).setToken(process.env.BOT_TOKEN);
 
 // Set global command
-rest.put(
-    Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID,
-    ),
-    {body: commands},
-)
+rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {body: commands})
     .then(() => console.log('Successfully registered commands'))
     .catch((e) => console.error(e));
